c5d449b1811f252dae0f1a3dc4a53ae1648881ec
XuXing <xux660@hotmail.com>
2022-09-27 14:46:21 +0800

    Add "bake then save" operator
    if file is not saved after point cache baking, 
    is_bake flag won't be saved, when open the file again, cache will be considered dirty and might get deleted.
    
    bake then save operator saves the file after baking is done, thus solving the problem.

--- XMisc.py
+++ XMisc.py
@@ -60,6 +60,28 @@ class XClearAllUnusedDataOp(bpy.types.Operator):
 
         return {'FINISHED'}
 
+# 按钮
+class XBakeThenSaveOp(bpy.types.Operator):
+    bl_idname = "xutil.bake_then_save"
+    bl_label = ""
+
+    @classmethod
+    def poll(cls, context):
+        return True
+
+    def execute(self, context):
+
+        #try:
+        #    bpy.ops.ptcache.bake(bake=True)
+        #finally:
+        #    bpy.ops.wm.save_mainfile()
+
+        bpy.ops.ptcache.bake(bake=True)
+        bpy.ops.wm.save_mainfile()
+
+        return {'FINISHED'}
+
+
 # 面板
 class XMiscPanel(bpy.types.Panel):
     bl_label = "Misc"
@@ -78,21 +100,39 @@ class XMiscPanel(bpy.types.Panel):
         row = self.layout.row()
         row.operator("xutil.clear_all_unused_data", text="Clear All Unused Data")
 
+# 物理模拟扩展
+def draw_phys_extras(self, context):
 
+    layout = self.layout
+
+    layout.separator()
+
+    row = layout.row()
+    row.label(text="XUtil:")
+
+    row = layout.row()
+    row.operator(XBakeThenSaveOp.bl_idname, text="Bake Then Save")
 
 # 模组初始化
 classes = (
     XClearSystemConsoleOp,
     XClearInfoWindowOp,
     XClearAllUnusedDataOp,
+    XBakeThenSaveOp,
     XMiscPanel,
 )
 
 def initialize():
     for cls in classes:
         bpy.utils.register_class(cls)
+    bpy.types.PHYSICS_PT_cloth_cache.append(draw_phys_extras)
+    bpy.types.PHYSICS_PT_softbody_cache.append(draw_phys_extras)
+    bpy.types.PARTICLE_PT_cache.append(draw_phys_extras)
 
 
 def uninitialize():
     for cls in classes:
         bpy.utils.unregister_class(cls)
+    bpy.types.PHYSICS_PT_cloth_cache.remove(draw_phys_extras)
+    bpy.types.PHYSICS_PT_softbody_cache.remove(draw_phys_extras)
+    bpy.types.PARTICLE_PT_cache.remove(draw_phys_extras)
